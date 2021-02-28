7517021089802561f33545d8c96211012c0dfbdd
Ksenia Kiseleva <69579994+kmkiseleva@users.noreply.github.com>
2021-03-01 00:32:04 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// БУРГЕР
 (function () {
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".nav-mobile");
@@ -19,6 +20,25 @@
   }
 })();
 
+// СТОИМОСТЬ УСЛУГ
+(function () {
+  let costRow = document.querySelectorAll(".cost__row");
+  const costArr = [...costRow];
+  let costMobile = document.querySelectorAll(".cost__mobile");
+  const costMobileArr = [...costMobile];
+  let costBurger = document.querySelectorAll(".cost__burger");
+  const costBurgerArr = [...costBurger];
+
+  for (let row of costArr) {
+    row.addEventListener("click", () => {
+      let index = costArr.indexOf(row);
+      costBurgerArr[index].classList.toggle("cost__burger_active");
+      costMobileArr[index].classList.toggle("cost__mobile_active");
+    });
+  }
+})();
+
+// СЛАЙДЕРЫ
 $(function () {
   $(".reviews__slider").owlCarousel({
     loop: true,
@@ -33,6 +53,29 @@ $(function () {
         items: 1,
       },
       850: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+});
+
+$(function () {
+  $(".reviews__slider-mob").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: [
+      '<img src="img/arrow_normal-l.svg">',
+      '<img src="img/arrow_normal-r.svg">',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
         items: 2,
       },
       1000: {
